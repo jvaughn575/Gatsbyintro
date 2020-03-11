72f1019686efb07cd764264401ed33111c057edb
James Vaughn <jvaughn575@gmail.com>
2020-03-11 11:42:27 -0500

    Added 404 css module, 404 page, and about page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './404.module.css';
+
+export default () => (
+    <div>
+        <h1>Page not found</h1>
+        
+        <p>
+            The page you are looking for does not exists.
+        </p>
+    </div>
+)
